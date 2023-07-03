c351476fd83604aec91d90bde145b649a4681f02
Kyuwoong Lee <kwooler@gmail.com>
2023-07-03 15:59:25 -0700

    Update main_test.py

--- main_test.py
+++ main_test.py
@@ -11,26 +11,17 @@ def test_main_1():
     datastr = '10 15 25 30 35\n20'
     sys.stdin = io.StringIO(datastr)
 
-    main.main()
     sys.stdout = sys.__stdout__
     print('Captured ', captureOut.getvalue())
     lines = captureOut.getvalue().split('\n')
     print(lines)
 
-    # regex_string = r'[\w,\W]*1'
-    # regex_string += r'[\w,\W]*3'
-    # regex_string += r'[\w,\W]*5'
-    # regex_string += r'[\w,\W]*'
-    # print(regex_string)
-    # res = re.search(regex_string, main.evenlist)
-    # assert res != None
-    # print(res.group())
-    assert main.main.numbers[0] == 10
-    assert main.main.numbers[1] == 15
-    assert main.main.numbers[2] == 20
-    assert main.main.numbers[3] == 25
-    assert main.main.numbers[4] == 30
-    assert main.main.numbers[5] == 35
+    numbers = main.getInput()
+    print(f'The original list values: {numbers}')
+    main.insertOne(numbers)
+    print(f'After insertion 20: {numbers}')
+
+    assert numbers == [10, 15, 20, 25, 30, 35]
 
 
 def test_main_2():
@@ -40,12 +31,15 @@ def test_main_2():
     datastr = '1 2 3 4 5\n6'
     sys.stdin = io.StringIO(datastr)
 
-    main.main()
     sys.stdout = sys.__stdout__
     print('Captured ', captureOut.getvalue())
     lines = captureOut.getvalue().split('\n')
     print(lines)
 
+    numbers = main.getInput()
+    print(f'The original list values: {numbers}')
+    main.insertOne(numbers)
+    print(f'After insertion 6: {numbers}')
     # regex_string = r'[\w,\W]*1'
     # regex_string += r'[\w,\W]*3'
     # regex_string += r'[\w,\W]*5'
@@ -54,12 +48,7 @@ def test_main_2():
     # res = re.search(regex_string, main.evenlist)
     # assert res != None
     # print(res.group())
-    assert main.main.numbers[0] == 1
-    assert main.main.numbers[1] == 2
-    assert main.main.numbers[2] == 3
-    assert main.main.numbers[3] == 4
-    assert main.main.numbers[4] == 5
-    assert main.main.numbers[5] == 6
+    assert numbers == [1, 2, 3, 4, 5, 6]
 
 
 def test_main_3():
@@ -69,12 +58,16 @@ def test_main_3():
     datastr = '1 2 3 4 5\n0'
     sys.stdin = io.StringIO(datastr)
 
-    main.main()
     sys.stdout = sys.__stdout__
     print('Captured ', captureOut.getvalue())
     lines = captureOut.getvalue().split('\n')
     print(lines)
 
+    numbers = main.getInput()
+    print(f'The original list values: {numbers}')
+    main.insertOne(numbers)
+    print(f'After insertion 0: {numbers}')
+
     # regex_string = r'[\w,\W]*1'
     # regex_string += r'[\w,\W]*3'
     # regex_string += r'[\w,\W]*5'
@@ -83,41 +76,10 @@ def test_main_3():
     # res = re.search(regex_string, main.evenlist)
     # assert res != None
     # print(res.group())
-    assert main.main.numbers[0] == 0
-    assert main.main.numbers[1] == 1
-    assert main.main.numbers[2] == 2
-    assert main.main.numbers[3] == 3
-    assert main.main.numbers[4] == 4
-    assert main.main.numbers[5] == 5
+    assert numbers == [0, 1, 2, 3, 4, 5]
 
 
 def test_main_4():
-    # captureOut = io.StringIO()
-    # sys.stdout = captureOut
-    # # datastr = '90\n10\n50\n40\n30'
-    # datastr = '1 2 3 4 5\n0'
-    # sys.stdin = io.StringIO(datastr)
-
-    # main.main()
-    # sys.stdout = sys.__stdout__
-    # print('Captured ', captureOut.getvalue())
-    # lines = captureOut.getvalue().split('\n')
-    # print(lines)
-
-    # regex_string = r'[\w,\W]*1'
-    # regex_string += r'[\w,\W]*3'
-    # regex_string += r'[\w,\W]*5'
-    # regex_string += r'[\w,\W]*'
-    # print(regex_string)
-    # res = re.search(regex_string, main.evenlist)
-    # assert res != None
-    # print(res.group())
-    # assert main.main.numbers[0] == 0
-    # assert main.main.numbers[1] == 1
-    # assert main.main.numbers[2] == 2
-    # assert main.main.numbers[3] == 3
-    # assert main.main.numbers[4] == 4
-    # assert main.main.numbers[5] == 5
     detect = 0
     with open('main.py') as f:
         for line in f:
@@ -125,4 +87,4 @@ def test_main_4():
                 detect = 1
             if line.find('sorted') != -1:
                 detect = 1
-    assert detect == 0
+    assert detect == 0, 'Do not use any sort functions'
